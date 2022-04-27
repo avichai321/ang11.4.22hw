34a8012b257cea49ae42970adc5a4cf460d880e7
avichai321 <77723644+avichai321@users.noreply.github.com>
2022-04-27 23:44:05 +0300

    update

--- src/app/people-info-comp/people-info-comp.component.ts
+++ src/app/people-info-comp/people-info-comp.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { PeopletableserviceService } from '../services/peopletableservice.service';
 
+
 @Component({
   selector: 'app-people-info-comp',
   templateUrl: './people-info-comp.component.html',
@@ -11,7 +12,7 @@ export class PeopleInfoCompComponent implements OnInit {
 insideinfo:any = {};
   constructor(private singlePerson:PeopletableserviceService) {
     this.insideinfo= singlePerson.getOnePerson(this.insideinfo.id).subscribe((per)=>{
-      this.insideinfo= per
+      this.insideinfo= per;
     })
    }
 
